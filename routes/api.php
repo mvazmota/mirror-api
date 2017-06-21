13bdb50b6f444809af7515a6e3dae4cd7157fd05
martin <m.vaz.mota@gmail.com>
2017-06-21 19:35:10 +0100

    added a lot of stuff, v1.0

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>'user'], function(){
+    Route::get('', ['uses' => 'Users@getUsers']);
+});
+
+Route::group(['prefix'=>'list'], function(){
+    Route::get('', ['uses' => 'Lists@getLists']);
+    Route::post('', ['uses' => 'Lists@createLists']);
+//    Route::delete('', ['uses' => 'Lists@removeLists']);
+});
+
+Route::group(['prefix'=>'product'], function(){
+    Route::get('{id}', ['uses' => 'Products@getProducts']);
+    Route::post('', ['uses' => 'Products@createProducts']);
+//    Route::delete('{id}', ['uses' => 'Products@removeProducts']);
+});
+
+Route::group(['prefix'=>'task'], function(){
+    Route::get('', ['uses' => 'Tasks@getTasks']);
+});
+
+Route::group(['prefix'=>'playlist'], function(){
+    Route::get('{id}', ['uses' => 'Playlists@getPlaylist']);
+});
+
+Route::group(['prefix'=>'mirror'], function(){
+    Route::get('{id}', ['uses' => 'Mirrors@getMirror']);
+});
